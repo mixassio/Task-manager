9311b44548bc8cba0f6f65ed05f281b058450337
mixassio <mixail.bondarev@gmail.com>
2018-10-06 20:41:04 +0300

    new

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+
+require("bootstrap/dist/css/bootstrap.min.css");
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL3NyYy9pbmRleC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUFBIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0ICdib290c3RyYXAvZGlzdC9jc3MvYm9vdHN0cmFwLm1pbi5jc3MnO1xuIl19
