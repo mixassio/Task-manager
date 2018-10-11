5ee346f58daff3152eafbd8ca6d2fbe960a39518
mixassio <mixail.bondarev@gmail.com>
2018-10-11 16:56:45 +0300

    orm

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 import welcome from './welcome';
+import users from './users';
 
 const controllers = [
   welcome,
+  users,
 ];
 
 export default (router, container) => controllers.forEach(f => f(router, container));
